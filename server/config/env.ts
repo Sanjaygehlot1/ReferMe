1017899137237819dcc9cf5a455c740af5d58107
Sanjaygehlot1 <sanjaygehlot1695@gmail.com>
2025-11-05 22:11:30 +0530

    fix: minor fix

--- server/config/env.ts
+++ server/config/env.ts
@@ -1,6 +1,6 @@
 export const envConfig = {
     PORT : process.env.PORT || 8000,
-    MONGO_URI : "mongodb+srv://Sanjay:Nothing@cluster0.00ci6ob.mongodb.net",
+    MONGO_URI : process.env.MONGO_URI,
     JWT_SECRET : process.env.JWT_SECRET,
     JWT_EXPIRY : process.env.JWT_EXPIRY
 
